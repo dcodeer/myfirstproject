4eaaad0621994ed9566a7998cb80ec60ec7fb8f4
dcodeer <dip.chakrabarti@gmail.com>
2025-04-26 16:57:18 +0530

    Simple Swapping in python

---
+++ Swap.py
@@ -0,0 +1,15 @@
+print("This is to test the swap functionality\n")
+print("Enter the first number:")
+num1 = int(input())
+print("Enter the second number:")
+num2 = int(input())
+print("Before swapping:")
+print("num1 =", num1)
+print("num2 =", num2)
+num3 = num1
+num1 = num2
+num2 = num3
+print("After swapping:")
+print("num1 =", num1)
+print("num2 =", num2)
+print("\nSwapping completed successfully")
